7f35351df510b0d01efa9ed3aafdcb96b804850c
admin <ayush2411@gmail.com>
2024-05-31 13:31:51 +0530

    fix

--- projects/node_calculator/calculator.spec.js
+++ projects/node_calculator/calculator.spec.js
@@ -3,5 +3,5 @@ const calculator = new Calculator();
 
 it('1 + 2 = 3 >', () => {
     const expected = 3;
-    expect(calculator.add(1,2)).toEqual(3);
+    expect(calculator.add(1,2)).toEqual(expected);
 });
